48fd9448514e72d80e6b6e47078354974bf2004d
sadaf-991 <mssadafansari@gmail.com>
2024-07-12 14:53:20 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Board from './Tic-Tac-ToeGame/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
